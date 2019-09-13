02d46d39f8f3e7e3da4068e2176b05b2312c55dd
ZOFFO2014 <50304959+ZOFFO2014@users.noreply.github.com>
2019-09-13 04:27:48 +0300

    Исправлены баги

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 //Подключаем модули галпа
 const gulp = require('gulp');
 const concat = require('gulp-concat');
@@ -9,80 +9,85 @@ const del = require('del');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
-const sourcemaps = require('gulp-sourcemaps');
+// const sourcemaps = require('gulp-sourcemaps');
+
 //Порядок подключения css файлов
 const cssFiles = [
-    './src/css/main.css'
+   './src/css/main.css'
 ]
 //Порядок подключения js файлов
 const jsFiles = [
-    './src/js/lib.js',
-    './src/js/main.js']
+   './src/js/lib.js',
+   './src/js/main.js'
+]
 
 //Таск на стили CSS
 function styles() {
-    //Шаблон для поиска файлов CSS
-    //Всей файлы по шаблону './src/css/**/*.css'
-    return gulp.src(cssFiles)
-        //Объединение файлов в один
-        .pipe(concat('style.css'))
-        //Добавить префиксы
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 2 versions'],
-            cascade: false
-        }))
-        //Минификация CSS
-        .pipe(cleanCSS({
-            level: 2
-        }))
-        //Выходная папка для стилей
-        .pipe(gulp.dest('./build/css'))
-        .pipe(browserSync.stream());
+   //Шаблон для поиска файлов CSS
+   //Всей файлы по шаблону './src/css/**/*.css'
+   return gulp.src(cssFiles)
+      //Объединение файлов в один
+      .pipe(concat('style.css'))
+      //Добавить префиксы
+      .pipe(autoprefixer({
+         overrideBrowserslist: ['last 2 versions'],
+         cascade: false
+      }))
+      //Минификация CSS
+      .pipe(cleanCSS({
+         level: 2
+      }))
+      //Выходная папка для стилей
+      .pipe(gulp.dest('./build/css'))
+      .pipe(browserSync.stream());
 }
 
 //Таск на скрипты JS
 function scripts() {
-    //Шаблон для поиска файлов JS
-    //Всей файлы по шаблону './src/js/**/*.js'
-    return gulp.src(jsFiles)
-        //Объединение файлов в один
-        .pipe(concat('script.js'))
-        //Минификация JS
-        .pipe(uglify({
-            toplevel: true
-        }))
-        //Выходная папка для скриптов
-        .pipe(gulp.dest('./build/js'))
-        .pipe(browserSync.stream());
+   //Шаблон для поиска файлов JS
+   //Всей файлы по шаблону './src/js/**/*.js'
+   return gulp.src(jsFiles)
+      //Объединение файлов в один
+      .pipe(concat('script.js'))
+      //Минификация JS
+      // .pipe(uglify({
+      //    toplevel: true
+      // }))
+      //Выходная папка для скриптов
+      .pipe(gulp.dest('./build/js'))
+      .pipe(browserSync.stream());
 }
 
 //Удалить всё в указанной папке
 function clean() {
-    return del(['build/*'])
+   return del(['build/*'])
 }
 
 //Просматривать файлы
 function watch() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    //Следить за CSS файлами
-    gulp.watch('./src/css/**/*.css', styles)
-    //Следить за JS файлами
-    gulp.watch('./src/js/**/*.js', scripts)
-    //При изменении HTML запустить синхронизацию
-    gulp.watch("./*.html").on('change', browserSync.reload);
+   browserSync.init({
+      server: {
+         baseDir: "./"
+      }
+   });
+   //Следить за SCSS файлами
+   gulp.watch('./src/scss/**/*.scss', cssPrepros)
+   //Следить за CSS файлами
+   gulp.watch('./src/css/**/*.css', styles)
+   //Следить за JS файлами
+   gulp.watch('./src/js/**/*.js', scripts)
+   //При изменении HTML запустить синхронизацию
+   gulp.watch("./*.html").on('change', browserSync.reload);
 }
+
 
 //Компилируем sass
 function cssPrepros() {
-    return gulp.src('./src/sass/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write('../css/'))
-        .pipe(gulp.dest('./src/css'));
+   return gulp.src('./src/scss/**/*.scss')
+      // .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      // .pipe(sourcemaps.write('../css/'))
+      .pipe(gulp.dest('./src/css'));
 }
 //Таск вызывающий функцию cssPrepros
 gulp.task('sass', cssPrepros);
@@ -95,6 +100,6 @@ gulp.task('del', clean);
 //Таск для отслеживания изменений
 gulp.task('watch', watch);
 //Таск для удаления файлов в папке build и запуск styles и scripts
-gulp.task('build', gulp.series(clean, "sass", gulp.parallel(styles, scripts)));
+gulp.task('build', gulp.series(clean, gulp.parallel(cssPrepros, styles, scripts)));
 //Таск запускает таск build и watch последовательно
-gulp.task('dev', gulp.series('build', 'watch')); 
+gulp.task('dev', gulp.series('build', 'watch'));
